1fe676a200a719cbca923e800ed180e4973a0601
remindiods <ayseekux@gmail.com>
2020-03-09 21:15:23 +0300

    Added derivtives calculation to NN

--- neural network 5 neurons/network2_gender_prediction.py
+++ neural network 5 neurons/network2_gender_prediction.py
@@ -57,5 +57,82 @@ class NeuralNet:
                 o1 = sigmoid(sum_o1)
                 y_pred = o1
 
+                """
+                Training of network.
+                Lets use SGD (Stochastic Gradient) method, which says how we should change 
+                weights and biases to make minimize losses.
+                Yhe main equation is: w1 <- w1 - n * (dL/dw1)
+                If dL/dw1 > 0, then w1 decreases, what causes that L1 decreases too.
+                If dL/dw1 < 0, then w1 increases, what causes that L1 increases too.
+                
+                If we use this to every weight, losses will continuously decrease.
+                
+                What we do.
+                1. Take one dataset.
+                2. Calculate all derivatives by weight or  by bias.
+                3. Use renovation equation for every weight or bias.
+                4. Return to first point.
+                """
                 # Derivatives calculation
+                d_L_d_y_pred = - 2 * (y_true - y_pred)
+
+                # Neuron o1
+                d_y_pred_d_w5 = h1 * deriv_sigmoid(sum_o1)
+                d_y_pred_d_w6 = h2 * deriv_sigmoid(sum_o1)
+                d_y_pred_d_b3 = deriv_sigmoid(sum_o1)
+
+                d_y_pred_d_h1 = self.w5 * deriv_sigmoid(sum_o1)
+                d_y_pred_d_h2 = self.w6 * deriv_sigmoid(sum_o1)
+
+                # Neuron h1
+                d_h1_d_w1 = x[0] * deriv_sigmoid(sum_h1)
+                d_h1_d_w2 = x[1] * deriv_sigmoid(sum_h1)
+                d_h1_d_b1 = deriv_sigmoid(h1)
+
+                # Neuron h2
+                d_h2_d_w3 = x[0] * deriv_sigmoid(sum_h2)
+                d_h2_d_w4 = x[1] * deriv_sigmoid(sum_h2)
+                d_h2_d_b2 = deriv_sigmoid(h2)
+
+                # Renew weight and biases
+                # Neuron h1
+                self.w1 -= learn_rate * d_L_d_y_pred * d_y_pred_d_h1 * d_h1_d...
+                self.w2 -= learn_rate * d_L_d_y_pred * d_y_pred_d_h1 * d_h1_d...
+                self.b1 -= learn_rate * d_L_d_y_pred * d_y_pred_d_h1 * d_h1_d...
+
+                # Neuron h2
+                self.w1 -= learn_rate * d_L_d_y_pred * d_y_pred_d_h1 * d_h1_d...
+                self.w2 -= learn_rate * d_L_d_y_pred * d_y_pred_d_h1 * d_h1_d...
+                self.b1 -= learn_rate * d_L_d_y_pred * d_y_pred_d_h1 * d_h1_d...
+
+                # Neuron o1
+                self.w3 -= learn_rate * d_L_d_y_pred * d_y_pred_d_h2 * d_h2_d...
+                self.w4 -= learn_rate * d_L_d_y_pred * d_y_pred_d_h2 * d_h2_d...
+                self.b2 -= learn_rate * d_L_d_y_pred * d_y_pred_d_h2 * d_h2_d...
+
+                # Calculate common loss in the end of every phase
+                if epoch % 10 == 0:
+                    y_preds = np.apply_along_axis(self.feedforward, 1, data)
+                    loss = mse_loss(all_y_trues, y_preds)
+                    print('epoch %d loss: %.3f' % (epoch, loss))
+
+
+# Dataset definition
+data = np.array([
+    [-2, -1], #Alice
+    [25, 6], #Bob
+    [17, 4], #Cecil
+    [-15, -6], #Diana
+])
+
+all_y_trues = np.array([
+    1, #Alice
+    0, #Bob
+    0, #Cecil
+    1, #Diana
+])
+
+# Train NN
+network = NeuralNet()
+network.train(data, all_y_trues)
 
